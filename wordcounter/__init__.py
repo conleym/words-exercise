41f9a7dc5d92d1054c71f90f007e1f188be4c4e8
Mike Conley <conasdf@gmail.com>
2017-09-04 14:12:20 -0400

    Add type annotations and docs to word_counter module.

--- wordcounter/__init__.py
+++ wordcounter/__init__.py
@@ -1,2 +1,6 @@
+import typing
+
+StreamOrPath = typing.Union[typing.IO[str], str]
+
 DEFAULT_ENCODING = 'utf-8'
 DEFAULT_ASCII_ONLY = False
--- wordcounter/word_counter.py
+++ wordcounter/word_counter.py
@@ -1,40 +1,44 @@
 import io
+from typing import Dict, IO, Iterable
 
-import wordcounter
-from wordcounter import word_tokenizer
+from wordcounter import (DEFAULT_ASCII_ONLY, DEFAULT_ENCODING, StreamOrPath,
+                         word_tokenizer)
 
 
-def merge_counts(totals, new_counts):
+def merge_counts(totals: Dict[str, int], new_counts: Dict[str, int]):
+    """Merge counts from a source into a running total over several sources."""
     for k, v in new_counts.items():
         totals[k] = totals.get(k, 0) + v
 
 
-def count_words(streams_or_paths, counter):
-    totals = {}
-    for stream_or_path in streams_or_paths:
-        merge_counts(totals, counter.count_words(stream_or_path))
-    return totals
-
-
-def update_counts(word_counts, current_word):
-    current_word_count = word_counts.get(current_word, 0)
-    word_counts[current_word] = current_word_count + 1
-
-
 class WordCounter:
-    """Counts the words in a file."""
-    def __init__(self, encoding=wordcounter.DEFAULT_ENCODING,
-                 ascii_only=wordcounter.DEFAULT_ASCII_ONLY):
+    """Counts the words in a body of text."""
+    def __init__(self, encoding: str=DEFAULT_ENCODING,
+                 ascii_only: str=DEFAULT_ASCII_ONLY):
+        """Create an instance.
+
+        :param encoding: The text encoding to use when opening files. Unused for
+         streams.
+        :param ascii_only: If True, count only ASCII alphanumeric character
+         sequences as words. Otherwise, count Unicode alphanumeric sequences as
+         words.
+        """
         self._ascii_only = ascii_only
         self._encoding = encoding
 
-    def _words(self, stream):
+    def _words(self, stream: IO[str]):
         if self._ascii_only:
             return word_tokenizer.WordTokenizer(stream,
                                                 word_tokenizer.is_ascii_alnum)
         return word_tokenizer.WordTokenizer(stream)
 
-    def count_words(self, stream_or_path):
+    def count_words(self, stream_or_path: StreamOrPath) -> Dict[str, int]:
+        """Count words in the given input source.
+
+        :param stream_or_path: the input source.
+        :return: a dict mapping each word found to the number of times it was
+         found in the input source.
+        """
         if isinstance(stream_or_path, str):
             stream = io.open(stream_or_path, encoding=self._encoding)
         else:
@@ -43,5 +47,21 @@ class WordCounter:
         with stream:
             words = self._words(stream)
             for word in words:
-                update_counts(word_counts, word.lower())
+                # Count is case-insensitive. Arbitrarily convert to lowercase.
+                word = word.lower()
+                word_counts[word] = word_counts.get(word, 0) + 1
         return word_counts
+
+
+def count_words(streams_or_paths: Iterable[StreamOrPath], counter: WordCounter):
+    """Count words in one or more input sources and return total counts.
+
+    :param streams_or_paths: the input sources.
+    :param counter: the WordCounter to use.
+    :return: a dict mapping each word found to the number of times it was found
+     in all the input sources.
+    """
+    totals = {}
+    for stream_or_path in streams_or_paths:
+        merge_counts(totals, counter.count_words(stream_or_path))
+    return totals
